34dc5b83cfbcf0eda6fee6365adfff4141b1eebb
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-01-28 02:48:23 -0800

    added css, wrote rough draft of JavaScript

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Day-Planner
+by Jenny Detmering
+
+## Description
+
+## Installation 
+
+## Deployment
+
+## Picture of Project
+
+## Technologies
+
+## Challenges
+1/20/2021-created files, started READ.ME
+1/23/2021-read documentation Jquery, Luxon, Bootstrap, Javascript
+1/26/2021-outlining on paper my approach to the solution. I am looking to start
+with the smallest hard thing first. 
+1/27/2021-wrote the JavaScript rough draft.
+
+
+## Contributors
+W3 Schools, Jquery, Luxone, Bootstrap, Javascript
---
+++ script.js
@@ -0,0 +1,73 @@
+//Javascript
+
+//Display Date
+//var today = luxon.DateTime.local();
+//luxon.DateTime.local() = document.getElementById(currentDay);
+//currentDay.innerHTML = luxon.DateTime; 
+
+
+// Display Hour - need to make this match up to current time somehow with Luxon
+//#1
+//var nowhour = luxon.hour
+//document.getElementbyClassName("hour")
+//hour.innerHTML = document.write(nowhour)
+
+//#2
+//var hourblock = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+//var hourlabel = document.getElementsByClassName(hour);
+//hour.innterHTML = function showBlockHour(); (loop through array to display hour in class hour spot)
+//variables maybe need to go into the function
+//function showHourBlockHour() = {
+//     for (i=o; i < 8, i++);
+//     hour.innerHTML = document.write(hourblock());
+// };
+ // might need to use this in array instead // var currenthour = luxon.DateTime.local();
+
+
+//click on hour block button
+//alert to form input reads info on how to save appointment in time block
+//user clicks save or submit button and hour block is updated with users new info
+// function makeEvent() = {
+//   var event = document.getElementsByClassName(textarea);
+//   console.log(event)
+//   event.push("text area");
+//   document.getElementById("usersEvent").innerHTML = event;
+// };
+ 
+
+// button brings up text area for user to make and save event
+//iterate over hour block array adding this to each value
+//document.getElementById("saveBtn i:hover").addEventListener("click", makeEvent());
+
+
+//attach all buttons to makeEvent() maybe it does this already?
+//
+//function eventButtons() = {
+//var nowhourblock = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"];
+//for (i=o; i < 10, i++);
+//makeEvent()
+//};
+
+
+//changes color of hourblocks based on current time. 
+
+//function colorcode() = {
+
+// var nowhourblock = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]; miitary time
+// var currentluxhour = luxon.DateTime.local();
+
+// var past = if nowhourblock < currentluxhour 
+//then 
+//setAttribute("past");
+
+// var present = if currentluxhour === nowhourblock
+//then
+//setAttribute("present");
+
+
+// var future = if nowhourblock > currentluxhour
+//then
+//setAttribute("future");
+//};
+
+    
